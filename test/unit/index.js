e5549a202eac5804aeee311d43530d8c71954752
crossjs <liwenfu@crossjs.com>
2017-01-14 03:39:17 +0800

    update tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -12,16 +12,12 @@ global.triggerTouchEvents = triggerTouchEvents
 global.assert = chai.assert
 global.expect = chai.expect
 
-// Reset styles
-document.body.style.margin = '0px'
-document.body.style.padding = '0px'
-
 // require all test files (files that ends with .spec.js)
-const testsContext = require.context('./', true, /\.spec\.js$/)
+const testsContext = require.context('./', true, /^\.[/\\]((?!node_modules).)*\.spec\.js$/)
 testsContext.keys().forEach(testsContext)
 
 // require all src files except index.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const componentsContext = require.context('../../', true, /^(core|styles)\/.*\.(js|vue)$/)
+const componentsContext = require.context('../../', true, /^\.[/\\]((?!test|index|coverage|node_modules).)*(\.js)$/)
 componentsContext.keys().forEach(componentsContext)
