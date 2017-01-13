c7f12ac03897925cddf26fd395c6931362d044ec
crossjs <liwenfu@crossjs.com>
2017-01-14 01:31:34 +0800

    npm publish

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Directives for PLATO
+# Vue Directives for [PLATO](https://github.com/crossjs/plato)
 
-Mobile first
+> Mobile first
+
+[![Travis](https://img.shields.io/travis/crossjs/directives.svg?style=flat-square)](https://travis-ci.org/crossjs/directives)
+[![Coveralls](https://img.shields.io/coveralls/crossjs/directives.svg?style=flat-square)](https://coveralls.io/github/crossjs/directives)
+[![dependencies](https://david-dm.org/crossjs/directives.svg?style=flat-square)](https://david-dm.org/crossjs/directives)
+[![devDependency Status](https://david-dm.org/crossjs/directives/dev-status.svg?style=flat-square)](https://david-dm.org/crossjs/directives?type=dev)
+[![NPM version](https://img.shields.io/npm/v/plato-directives.svg?style=flat-square)](https://npmjs.org/package/plato-directives)
---
+++ test/unit/index.js
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import sinonChai from 'sinon-chai'
+import { triggerHTMLEvents, triggerMouseEvents, triggerTouchEvents } from './utils'
+
+localStorage.clear()
+
+chai.use(sinonChai)
+
+global.triggerHTMLEvents = triggerHTMLEvents
+global.triggerMouseEvents = triggerMouseEvents
+global.triggerTouchEvents = triggerTouchEvents
+global.assert = chai.assert
+global.expect = chai.expect
+
+// Reset styles
+document.body.style.margin = '0px'
+document.body.style.padding = '0px'
+
+// require all test files (files that ends with .spec.js)
+const testsContext = require.context('./', true, /\.spec\.js$/)
+testsContext.keys().forEach(testsContext)
+
+// require all src files except index.js for coverage.
+// you can also change this to match only the subset of files that
+// you want coverage for.
+const componentsContext = require.context('../../', true, /^(core|styles)\/.*\.(js|vue)$/)
+componentsContext.keys().forEach(componentsContext)
