378a8dc4d1596ea8f14be721987f4e6b0f9679a1
ileenhow <ileenhow@gmail.com>
2017-04-12 13:08:41 +0800

    use createEvent as fallback for Event constructor

--- _create-event.js
+++ _create-event.js
@@ -1,3 +1,15 @@
-export default (name, options, mixins) => {
-  return Object.assign(new Event(name, options), mixins)
-}
+ export default (name, options, mixins) => {  
+  let event  
+  try {  
+    // Not supported in some versions of Android's old WebKit-based WebView  
+    // use document.createEvent() instead  
+    event = new Event(name, options)  
+  } catch (e) {  
+    if (!options) {  
+      options = {}  
+    }  
+    event = document.createEvent('CustomEvent')  
+    event.initCustomEvent(name, !!options.bubbles, !!options.cancelable)  
+  }  
+  return Object.assign(event, mixins)  
+} 
